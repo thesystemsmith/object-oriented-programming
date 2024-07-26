73fd12ee98fb157a02c9f77fda61edda3d84251f
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-07-26 16:07:39 +0530

    fix initialisation

--- classesAndObjects/Main.java
+++ classesAndObjects/Main.java
@@ -3,7 +3,7 @@ package classesAndObjects;
 public class Main {
     public static void main(String[] args) {
         // Creating an object of Car
-        Car myCar = new Car();
+        Car myCar = new Car("toyota", "camari", 2024);
         
         // Assigning values to fields
         myCar.make = "Toyota";
